b9a286b8d94da0b4c017ebf518a766cf7a84cb60
Savina Ivanova <91665550+sisssona@users.noreply.github.com>
2023-05-17 19:41:01 +0300

    Add files via upload

---
+++ Loops-Practical-Tasks-master/FindLargestThreeValues/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FindLargestThreeValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int first = int.MinValue;
+            int second = int.MinValue;
+            int third = int.MinValue;
+            
+
+            for (int i = 0; i < n; i++)
+            {
+                int numberValues = int.Parse(Console.ReadLine());
+                
+                if (numberValues > first)
+                {
+                    third = second;
+                    second = first;
+                    first = numberValues;                   
+                }                               
+                else if (numberValues > second)
+                {
+                    third = second;
+                    second = numberValues;
+                }
+                else if (numberValues > third)
+                {
+                    third = numberValues;
+                }
+               
+
+            }
+            Console.WriteLine($"{first}, {second} and {third}");
+        }
+    }
+}
